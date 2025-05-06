deb9baae860354b66190e88990d235a4aec99f17
isaac2008-lab <Isaac11rodrigues17@gmail.com>
2025-05-06 10:26:03 -0300

    JAVA UPDATE
    java script adquirido

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,10 @@
+console.log("ola")
+console.log("DGBSSH")
+
+const nota1 = 10
+const nota3 = 10 
+const nota2 = 9
+const mdia = (nota1 + nota2 + nota3)/3
+
+console.log(mdia)
+console.log("BR,BR,PATAPIM")
