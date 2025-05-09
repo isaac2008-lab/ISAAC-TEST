d818dcd4f9b820c3c902e99fab7ec194dd17f6ef
isaac2008-lab <Isaac11rodrigues17@gmail.com>
2025-05-09 07:47:41 -0300

    java
    java

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,5 +1,9 @@
-console.log("ola")
-console.log("DGBSSH")
+let mensagem1 = "OLA"
+let mensagem2 = "a^p = p (mod p)"
+let mensagem3 = "Se p não q , p q ?"
+
+console.log(mensagem1)
+console.log(mensagem2)
 
 const nota1 = 10
 const nota3 = 10 
@@ -7,4 +11,16 @@ const nota2 = 9
 const mdia = (nota1 + nota2 + nota3)/3
 
 console.log(mdia)
-console.log("BR,BR,PATAPIM")
+console.log(mensagem3)
+
+let exemplo1 = "1"
+let exemplo2 = 1
+console.log(exemplo1+exemplo1)
+console.log(exemplo2+exemplo2)
+
+const nome = "WENDE"
+const nome2 = "MOCAMBINHO"
+const nome3 = `www.${nome}${nome2}.com.br`
+console.log(nome3)
+
+
