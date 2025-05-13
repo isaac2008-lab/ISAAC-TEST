74a00208c439c5f0aa4efc6e97ebbeebfc2e20f8
isaac2008-lab <Isaac11rodrigues17@gmail.com>
2025-05-13 10:27:20 -0300

    papa
    1

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -5,12 +5,8 @@ let mensagem3 = "Se p não q , p q ?"
 console.log(mensagem1)
 console.log(mensagem2)
 
-const nota1 = 10
-const nota3 = 10 
-const nota2 = 9
-const mdia = (nota1 + nota2 + nota3)/3
 
-console.log(mdia)
+
 console.log(mensagem3)
 
 let exemplo1 = "1"
@@ -22,5 +18,41 @@ const nome = "WENDE"
 const nome2 = "MOCAMBINHO"
 const nome3 = `www.${nome}${nome2}.com.br`
 console.log(nome3)
+
+
+console.log(5+3)
+console.log(10 % 3)
+
+
+let preco = 20
+let quantidade = 3
+let total = preco*quantidade
+console.log(`Total a pagar : R$${total}`)
+
+
+const nota1 = 10
+const nota3 = 10 
+const nota2 = 8.5
+const mdia = (nota1 + nota2 + nota3)/3
+console.log(`A média do aluno foi: ${mdia}`)
+
+
+let a = 10
+a += 5
+console.log(a)
+
+
+
+let saldo = 100
+saldo -= 20
+console.log("Saldo atual " , saldo)
+
+
+
+let quantia = 0
+quantia += 50
+quantia += 50
+quantia -= 30
+console.log(quantia )
 
 
